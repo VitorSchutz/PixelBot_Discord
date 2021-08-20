ab9fe25ce44a592b7ba1da3772b09e534885f261
Vitor Schutz <76212597+VitorSchutz@users.noreply.github.com>
2021-08-20 16:04:01 -0300

    Music Bot

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ client = commands.Bot(command_prefix = ">", case_insenstive = True)
 
 @client.event
 async def on_ready():
-    print('Entramos como (0.user)'.format(client))
+    print('Estamos Online'.format(client))
 
+#Musica
 @client.command()
-async def Guigay(ctx):
-    await ctx.send(f'O Gui foi chamado de gay dnv pelo {ctx.author}')
+async def join(ctx):
+    Ucanal = author.voice.voice_channel
+    await ctx.join_voice_channel(Ucanal)
 
+
+#data
+@client.command()
+async def Data(ctx):
+    await ctx.send(f'Ainda não temos uma data para o RPG {ctx.author}')
+
+# Rolagem de dados
 @client.command()
 async def d(ctx, numero):
     dado = random.randint(1,int(numero))
